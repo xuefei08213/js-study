0c7f901380cbe81140496f6cd14ab04f3137e610
Xue Fei <1067031954@qq.com>
2023-01-01 12:20:46 +0800

    add code challenge

---
+++ code-challenge-1/script.js
@@ -0,0 +1,24 @@
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let bmiMark = massMark / heightMark ** 2;
+let bmiJohn = massJohn / heightJohn ** 2;
+
+let markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI)
+
+
+massMark = 95;
+heightMark = 1.88;
+
+massJohn = 85;
+heightJohn = 1.76;
+
+bmiMark = massMark / heightMark ** 2;
+bmiJohn = massJohn / heightJohn ** 2;
+
+markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI)
