2af5000e799c395aaa821d8e5f5dfbcf76d74634
Xue Fei <1067031954@qq.com>
2023-01-02 10:39:56 +0800

    add boolean-logic

---
+++ boolean-logic/script.js
@@ -0,0 +1,10 @@
+// and or not
+
+const A = false;
+const B = true;
+
+console.log(!A); // true
+console.log(A && B); // false
+console.log(A || B); // true
+console.log(!A && B); // true
+console.log(A || !B); //false
