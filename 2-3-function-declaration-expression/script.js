abb72c942579ee4e77c1ad429ce70b025ba1c78d
Xue Fei <1067031954@qq.com>
2023-02-06 13:32:00 +0800

    docs: 添加函数声明以及表达式部分的注释

--- 2-3-function-declaration-expression/script.js
+++ 2-3-function-declaration-expression/script.js
@@ -1,9 +1,11 @@
+// Function declaration
 function calcAge1(birthYear) {
   return 2037 - birthYear;
 }
 
 const age1 = calcAge1(1990);
 
+// Function expression
 const calcAge2 = function (birthYear) {
   return 2037 - birthYear;
 };
