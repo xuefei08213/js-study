c6e74a66c9c4baa7394057c056f41286555606b4
Xue Fei <1067031954@qq.com>
2023-03-25 19:53:27 +0800

    feat: 添加面试代码

--- js-interview/index.js
+++ js-interview/index.js
@@ -1,10 +1,23 @@
-setTimeout(() => console.log(1), 0);
+// setTimeout(() => console.log(1), 0);
 
-console.log(2);
+// console.log(2);
 
-new Promise((res) => {
-  console.log(3);
-  res();
-}).then(() => console.log(4));
+// new Promise((res) => {
+//   console.log(3);
+//   res();
+// }).then(() => console.log(4));
 
-console.log(5);
+// console.log(5);
+
+// function foo(bar, getBar = () => bar) {
+//   var bar = 10;
+//   console.log(getBar());
+// }
+
+// foo(5);
+
+console.log('index.js');
+
+import { sum } from './helper.js';
+
+console.log(sum(1, 2));
